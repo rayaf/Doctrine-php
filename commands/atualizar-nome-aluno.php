f0079a6288565f2c04ecbc08e580ab0e1eb23f6d
Ray Everton <ray.evaf@gmail.com>
2019-07-10 20:56:14 -0300

    craido comando para atualizar

---
+++ commands/atualizar-nome-aluno.php
@@ -0,0 +1,16 @@
+<?php
+use Estudo\Doctrine\Entity\Aluno;
+use Estudo\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$id = $argv[1];
+$novoNome = $argv[2];
+
+$aluno = $entityManager->find(Aluno::class, $id);
+$aluno->setNome($novoNome);
+
+$entityManager->flush();
