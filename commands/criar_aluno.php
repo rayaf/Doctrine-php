35118d64e8003d47f56a0989838b0ee939b18fa5
Ray Everton <ray.evaf@gmail.com>
2019-07-09 22:30:12 -0300

    movido commands para o lugar certo

--- commands/criar_aluno.php
+++ commands/criar_aluno.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Estudo\Doctrine\Entity\Aluno;
+use Estudo\Doctrine\Helper\EntityManagerFactory;
 
 $aluno = new Aluno();
 $aluno->setNome('Ray Everton');
